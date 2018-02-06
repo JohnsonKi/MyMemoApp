5c54b1047fec10587c52f3c8776788ae06cce030
jxm930 <jxm930@gmail.com>
2018-02-06 21:38:24 +0900

    feature done

---
+++ app/src/main/java/com/example/android/mymemoapp/MemoLoadFragment.java
@@ -0,0 +1,107 @@
+package com.example.android.mymemoapp;
+
+import android.app.ListFragment;
+import android.app.LoaderManager;
+import android.content.ContentUris;
+import android.content.Context;
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ListView;
+
+/**
+ * 保存されたメモの一覧用Fragment
+ */
+public class MemoLoadFragment extends ListFragment implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    // FragmentとActivity間はインターフェースを通してアクセスすることで、
+    // FragmentがActivityの実装に依存することを防ぐ
+    public interface MemoLoadFragmentListener {
+        void onMemoSelected(@Nullable Uri uri);
+    }
+
+    private static final int CURSOR_LOADER = 1;
+
+    private MemoAdapter mAdapter;
+
+    // Activityがインターフェースを実装しているかチェックする
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        if (!(context instanceof MemoLoadFragmentListener)) {
+            // ActivityがMemoLoadFragmentListenerを実装していない場合
+            throw new RuntimeException(context.getClass().getSimpleName()
+                    + " does not implement MemoLoadFragmentListener");
+        }
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        // Headerを追加する
+        View header = LayoutInflater.from(getActivity()).inflate(R.layout.memo_list_create, null);
+        getListView().addHeaderView(header);
+
+        // 空のAdapterをセットする
+        mAdapter = new MemoAdapter(getActivity(), null, true);
+        setListAdapter(mAdapter);
+
+        // Loaderを初期化する
+        getLoaderManager().restartLoader(CURSOR_LOADER, getArguments(), this);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+
+        // Loaderを廃棄
+        getLoaderManager().destroyLoader(CURSOR_LOADER);
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        if (position == 0) {
+            // Headerの場合
+            ((MemoLoadFragmentListener)getActivity()).onMemoSelected(null);
+
+        } else {
+
+            Uri selectedItem = ContentUris.withAppendedId(MemoProvider.CONTENT_URI, id);
+            ((MemoLoadFragmentListener)getActivity()).onMemoSelected(selectedItem);
+        }
+
+    }
+
+    @Override
+    public CursorLoader onCreateLoader(int id, Bundle args) {
+        if (id == CURSOR_LOADER) {
+            return new CursorLoader(getActivity(), MemoProvider.CONTENT_URI,
+                    null, null, null, MemoDBHelper.DATE_MODIFIED + " DESC");
+        }
+
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        if (loader.getId() == CURSOR_LOADER) {
+            mAdapter.swapCursor(data);
+            mAdapter.notifyDataSetChanged();
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        if (loader.getId() == CURSOR_LOADER) {
+            mAdapter.swapCursor(null);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/android/mymemoapp/SettingFragment.java
@@ -0,0 +1,95 @@
+package com.example.android.mymemoapp;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.text.TextUtils;
+
+import java.util.Collections;
+import java.util.Set;
+
+public class SettingFragment extends PreferenceFragment
+        implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+    // 変更イベントをActivityに通知する
+    public interface SettingFragmentListener {
+        void onSettingChanged();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // ファイル名を指定する
+        getPreferenceManager().setSharedPreferencesName(
+                SettingPrefUtil.PREF_FILE_NAME);
+
+        // Load the preferences from an XML resource
+        addPreferencesFromResource(R.xml.preferences);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        setTypefaceSummary(getPreferenceManager().getSharedPreferences());
+        setPrefixSummary(getPreferenceManager().getSharedPreferences());
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        getPreferenceScreen().getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        getPreferenceScreen().getSharedPreferences()
+                .unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(
+            SharedPreferences sharedPreferences, String key) {
+        // Activityを取得
+        Activity activity = getActivity();
+
+        // ActivityがSettingFragmentListenerを実装しているのであれば、通知する
+        if (activity instanceof SettingFragmentListener) {
+            SettingFragmentListener listener = (SettingFragmentListener)activity;
+
+            // Activityに変更通知
+            listener.onSettingChanged();
+        }
+
+        // サマリーに反映する
+        if (activity.getString(R.string.key_text_style).equals(key)) {
+            setTypefaceSummary(sharedPreferences);
+        } else if (activity.getString(R.string.key_file_name_prefix).equals(key)) {
+            setPrefixSummary(sharedPreferences);
+        }
+    }
+
+    private void setTypefaceSummary(SharedPreferences sharedPreferences) {
+        String key = getActivity().getString(R.string.key_text_style);
+
+        Preference preference = findPreference(key);
+
+        Set<String> selected = sharedPreferences.getStringSet(key, Collections.<String>emptySet());
+        preference.setSummary(TextUtils.join("/", selected.toArray()));
+    }
+
+    private void setPrefixSummary(SharedPreferences sharedPreferences) {
+        String key = getActivity().getString(R.string.key_file_name_prefix);
+
+        Preference preference = findPreference(key);
+
+        String prefix = sharedPreferences.getString(key, "");
+        preference.setSummary(prefix);
+    }
+
+}
